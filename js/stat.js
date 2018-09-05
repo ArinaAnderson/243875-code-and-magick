0173eb6f613c66b655a27ef6bc3094a53bd6b97e
Marina <iolsoOfuolso@gmail.com>
2018-09-05 10:36:34 -0700

    Исправлена функция нарезки текста, удален счетчик countOfBars, удалена строка в начале определения шрифта

--- js/stat.js
+++ js/stat.js
@@ -11,7 +11,7 @@ var cloudParams = {
   outlineColor: 'black',
   offset: 10,
   lineWidth: 1,
-  paddingTop: 18
+  paddingTop: 5
 };
 
 // text характеристики СОЗДАТЬ ОБЪЕКТ ТЕКСТ headingTextParams + метод построения нарезки текста
@@ -22,17 +22,17 @@ var textParams = {
   color: 'black',
   lineHeight: 1.375,
   marginLeft: 20,
-  maxLineWidth: 20
+  maxLineWidth: 200
 };
 
 // barchart характеристики
 var chartParams = {
   chartMarginLft: 40,
-  chartMarginTp: 5,
+  chartMarginTp: 30,
   barWidth: 40,
   barMaxHeight: 150,
   barMarginRght: 50,
-  barMarginTp: 0,
+  barMarginTp: 10,
   hue: 216,
   lightness: 50,
   getSaturation: function () {
@@ -54,21 +54,22 @@ function drawInfotable(ctx) {
 
 // функция добавления текста
 function renderHeading(ctx, text, fontSize, lineHeight, maxWidth) {
-  var linesAmount = Math.ceil(text.length / maxWidth);// var linesAmount = textParams.numberOfLines;
   var textArray = text.split(' ');
-  var countOfIndex = 0;
-  for (var j = 1; j <= linesAmount; j++) {
-    var line = '';
-    for (var i = countOfIndex; i < textArray.length; i++) {
-      var check = line + textArray[i];
-      if (check.length > maxWidth) {
-        break;
-      }
-      line = check + ' ';
-      countOfIndex++;
+  var count = textArray.length;
+  var addition = fontSize * lineHeight;
+  var line = '';
+  for (var i = 0; i < count; i++) {
+    var check = line + textArray[i] + ' ';
+    var checkWidth = ctx.measureText(check).width;
+    if (checkWidth > maxWidth) {
+      ctx.fillText(line, START_X + textParams.marginLeft, START_Y + cloudParams.paddingTop + addition);
+      line = textArray[i] + ' ';
+      addition += fontSize * lineHeight;
+    } else {
+      line = check;
     }
-    ctx.fillText(line.trim(), START_X + textParams.marginLeft, cloudParams.paddingTop + fontSize * lineHeight * j);
   }
+  ctx.fillText(line.trim(), START_X + textParams.marginLeft, START_Y + cloudParams.paddingTop + addition)
 }
 
 // функция поиска максимума
@@ -87,7 +88,7 @@ function buildBar(ctx, x, bottom, value, maxValue, text) {
   var barHeight = chartParams.barMaxHeight * value / maxValue;
   ctx.fillRect(x, bottom - barHeight, chartParams.barWidth, barHeight);
   ctx.fillStyle = textParams.color;
-  ctx.fillText(Math.floor(value), x, bottom - barHeight - chartParams.chartMarginTp);
+  ctx.fillText(Math.floor(value), x, bottom - barHeight - chartParams.barMarginTp);
   ctx.fillText(text, x, bottom + textParams.lineHeight * textParams.size);
 }
 
@@ -97,7 +98,7 @@ window.renderStatistics = function (ctx, names, times) {
 
   // построение текста
   ctx.fillStyle = textParams.color;
-  ctx.font = '' + textParams.size + 'px' + ' ' + textParams.family;
+  ctx.font = textParams.size + 'px' + ' ' + textParams.family;
   renderHeading(ctx, textParams.text, textParams.size, textParams.lineHeight, textParams.maxLineWidth);
 
   // построение гистограммы
@@ -105,12 +106,10 @@ window.renderStatistics = function (ctx, names, times) {
   var chartY = START_Y + cloudParams.paddingTop + textParams.lineHeight * textParams.size * Math.ceil(textParams.text.length / textParams.maxLineWidth) + chartParams.chartMarginTp;
   var chartX = START_X + chartParams.chartMarginLft;
   var chartBottom = chartY + textParams.lineHeight * textParams.size + chartParams.barMarginTp + chartParams.barMaxHeight;
-  var countOfBars = 0;
 
   for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = names[i] === 'Вы' ? 'red' : 'hsl(' + chartParams.hue + ', ' + chartParams.getSaturation() + '%' + ', ' + chartParams.lightness + '%)';
-    var barX = chartX + countOfBars * (chartParams.barWidth + chartParams.barMarginRght);
+    var barX = chartX + i * (chartParams.barWidth + chartParams.barMarginRght);
     buildBar(ctx, barX, chartBottom, times[i], maxTime, names[i]);
-    countOfBars++;
   }
 };
