5cd309df2470add626d51e282f3d761c99d2b50d
Marina <iolsoOfuolso@gmail.com>
2018-09-23 23:22:23 -0700

    перетаскивание окна диалога, задание 5-1

---
+++ js/dialog.js
@@ -0,0 +1,144 @@
+'use strict';
+(function () {
+  var COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+  var setup = document.querySelector('.setup');
+  var setupOpenBtn = document.querySelector('.setup-open');
+  var setupCloseBtn = setup.querySelector('.setup-close');
+  var setupUser = document.querySelector('[name = username]');
+  var setupToggler = setup.querySelector('.upload');
+  var wizardSetup = document.querySelector('.setup-wizard');
+  var wizardCoat = wizardSetup.querySelector('.wizard-coat');
+  var wizardCoatInput = document.querySelector('[name = coat-color]');
+  var wizardEyes = wizardSetup.querySelector('.wizard-eyes');
+  var wizardEyesInput = document.querySelector('[name = eyes-color]');
+  var wizardFireball = document.querySelector('.setup-fireball-wrap');
+  var wizardFireballInput = wizardFireball.querySelector('[name = fireball-color]');
+  var setupInitialCoord;
+  var body = document.querySelector('body');
+  var countsOfElemColors = {
+    'coat': 0,
+    'eyes': 0,
+    'fireball': 0
+  };
+
+  var changeColor = function (wizaerdElem, colorList, countKey, styleProperty) {
+    countsOfElemColors[countKey]++;
+    if (countsOfElemColors[countKey] === colorList.length) {
+      countsOfElemColors[countKey] = 0;
+    }
+    var value = colorList[countsOfElemColors[countKey]];
+    wizaerdElem.style[styleProperty] = value;
+    return value;
+  };
+
+  var setupEscPressHandler = function (evt) {
+    window.utils.isEscPress(evt, closeSetup);
+  };
+
+  var setupTogglerMouseDownHandler = function (evt) {
+    evt.preventDefault();
+    var startCoord = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    
+    var dragged = false;
+    var inputPicUnload = setup.querySelector('input[type="file"]');
+
+    var setupTogglerMouseMoveHandler = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoord.x - moveEvt.clientX,
+        y: startCoord.y - moveEvt.clientY
+      };
+
+      startCoord = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+    };
+
+    var setupTogglerMouseUpHandler = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', setupTogglerMouseMoveHandler);
+      document.removeEventListener('mouseup', setupTogglerMouseUpHandler);
+
+      if (dragged) {
+        var defaultClickHandler = function (evt) {
+          evt.preventDefault();
+          setupToggler.removeEventListener('click', defaultClickHandler);
+        };
+        setupToggler.addEventListener('click', defaultClickHandler );
+        console.log('cat');
+      } 
+    };
+
+    document.addEventListener('mousemove', setupTogglerMouseMoveHandler);
+    document.addEventListener('mouseup', setupTogglerMouseUpHandler);
+  };
+
+  var closeSetup = function () {
+    setup.classList.add('hidden');
+    document.removeEventListener('keydown', setupEscPressHandler);
+    setup.style.left = '50%';
+    setup.style.top = setupInitialCoord.y + 'px';
+  };
+
+  var openSetup = function () {
+    setup.classList.remove('hidden');
+    setupInitialCoord = {
+      y: setup.offsetTop
+    };
+
+    setupUser.addEventListener('focus', function () {
+      document.removeEventListener('keydown', setupEscPressHandler);
+    });
+    setupUser.addEventListener('blur', function () {
+      document.addEventListener('keydown', setupEscPressHandler);
+    });
+    document.addEventListener('keydown', setupEscPressHandler);
+
+    setupCloseBtn.addEventListener('click', function () {
+      closeSetup();
+    });
+    setupCloseBtn.addEventListener('keydown', function (evt) {
+      window.utils.isEnterPress(evt, closeSetup);
+    });
+
+    wizardCoat.addEventListener('click', function () {
+      wizardCoatInput.value = changeColor(wizardCoat, COAT_COLORS, 'coat', 'fill');
+    });
+    wizardEyes.addEventListener('click', function () {
+      wizardEyesInput.value = changeColor(wizardEyes, EYES_COLORS, 'eyes', 'fill');
+    });
+    wizardFireball.addEventListener('click', function () {
+      wizardFireballInput.value = changeColor(wizardFireball, FIREBALL_COLORS, 'fireball', 'backgroundColor');
+    });
+
+    setupToggler.addEventListener('mousedown', setupTogglerMouseDownHandler);
+  };
+
+  setupOpenBtn.addEventListener('click', function () {
+    openSetup();
+  });
+  setupOpenBtn.addEventListener('keydown', function (evt) {
+    window.utils.isEnterPress(evt, openSetup);
+  });
+
+  document.querySelector('.setup-similar').classList.remove('hidden');
+})();
---
+++ js/wizards-rendering.js
@@ -0,0 +1,33 @@
+'use strict';
+(function () {
+  var WIZARDS_NUMBER = 4;
+  var wizardsList = document.querySelector('.setup-similar-list');
+  var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+  function renderWizard(wizardData) {
+    var wizard = wizardTemplate.cloneNode(true);
+    wizard.querySelector('.setup-similar-label').textContent = wizardData.name;
+    wizard.querySelector('.wizard-coat').style.fill = wizardData.coatColor;
+    wizard.querySelector('.wizard-eyes').style.fill = wizardData.eyesColor;
+    return wizard;
+  }
+
+  function createWizardsList(number) {
+    var wizards = [];
+    for (var i = 0; i < number; i++) {
+      wizards.push(window.getWizardData());
+    }
+    return wizards;
+  }
+
+  function renderWizards(wizardsData) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < wizardsData.length; i++) {
+      fragment.appendChild(renderWizard(wizardsData[i]));
+    }
+    wizardsList.appendChild(fragment);
+  }
+
+  var wizards = createWizardsList(WIZARDS_NUMBER);
+  renderWizards(wizards);
+})();
